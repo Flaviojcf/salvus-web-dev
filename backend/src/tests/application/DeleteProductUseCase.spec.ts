347d3c42e33fedfcdbf0a6a58c5b24dc6ecf4b1d
Flávio Costa <flaviojcostafilho@gmail.com>
2024-07-12 01:50:32 -0300

    tests: Apply new mock for find product use case

--- backend/src/tests/application/DeleteProductUseCase.spec.ts
+++ backend/src/tests/application/DeleteProductUseCase.spec.ts
@@ -1,15 +1,18 @@
 import DeleteProductUseCase from '@/application/useCase/delete/DeleteProductUseCase'
 import type IProductRepository from '@/domain/repositories/IProductRepository'
 import type InputDeleteProductDTO from '@/application/useCase/delete/dto/DeleteProductDTO'
+import type FindProductUseCase from '@/application/useCase/find/FindProductUseCase'
 
 describe('Delete Product Use Case', () => {
   let sut: DeleteProductUseCase
   let mockProductRepository: IProductRepository
+  let mockFindProductUseCase: FindProductUseCase
 
   beforeEach(() => {
     mockProductRepository = { delete: jest.fn().mockResolvedValue(null) } as unknown as IProductRepository
+    mockFindProductUseCase = { execute: jest.fn().mockResolvedValue({ Id: 'product-id-to-delete' }) } as unknown as FindProductUseCase
 
-    sut = new DeleteProductUseCase(mockProductRepository)
+    sut = new DeleteProductUseCase(mockProductRepository, mockFindProductUseCase)
   })
 
   it('should delete a product', async () => {
@@ -17,10 +20,8 @@ describe('Delete Product Use Case', () => {
       Id: 'product-id-to-delete'
     }
 
-    jest.spyOn(mockProductRepository, 'delete').mockResolvedValueOnce()
-
     await sut.execute(input)
 
-    expect(mockProductRepository.delete).toHaveBeenCalledWith(input.Id)
+    expect(mockProductRepository.delete).toHaveBeenCalledWith('product-id-to-delete')
   })
 })
