d4fe7c8e239b77bf505b69f5b5211d512725e570
Flávio Costa <flaviojcostafilho@gmail.com>
2024-07-11 23:05:08 -0300

    feat: Create interface for all use cases

---
+++ backend/src/presentation/interfaces/UseCases.ts
@@ -0,0 +1,11 @@
+import type CreateProductUseCase from '@/application/useCase/create/CreateProductUseCase'
+import type FindProductUseCase from '@/application/useCase/find/FindProductUseCase'
+import type ListProductUseCase from '@/application/useCase/list/ListProductUseCase'
+import type UpdateProductUseCase from '@/application/useCase/update/UpdateProductUseCase'
+
+export interface UseCases {
+  createProduct: CreateProductUseCase
+  // findProduct: FindProductUseCase
+  // listProduct: ListProductUseCase
+  // updateProduct: UpdateProductUseCase
+}
