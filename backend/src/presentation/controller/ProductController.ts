13e191f445e1075a11e6dd909d380a9f76ab1389
Flávio Costa <flaviojcostafilho@gmail.com>
2024-07-12 02:44:07 -0300

    refactor: Change method name

--- backend/src/presentation/controller/ProductController.ts
+++ backend/src/presentation/controller/ProductController.ts
@@ -4,11 +4,13 @@ import { type InputCreateProductDTO } from '@/application/useCase/create/dto/Cre
 import type FindProductUseCase from '@/application/useCase/find/FindProductUseCase'
 import { CreateProductValidator } from '../validations/createProductValidator'
 import type DeleteProductUseCase from '@/application/useCase/delete/DeleteProductUseCase'
+import type ListProductUseCase from '@/application/useCase/list/ListProductUseCase'
 
 export default class ProductController {
   constructor (private readonly createProductUseCase: CreateProductUseCase,
     private readonly findProductUseCase: FindProductUseCase,
-    private readonly deleteProductUseCase: DeleteProductUseCase
+    private readonly deleteProductUseCase: DeleteProductUseCase,
+    private readonly listProductUseCase: ListProductUseCase
   ) {}
 
   async create (request: Request, response: Response): Promise<Response> {
@@ -29,17 +31,69 @@ export default class ProductController {
 
   async find (request: Request, response: Response): Promise<Response> {
     const { id } = request.params
+    try {
+      const result = await this.findProductUseCase.execute({ Id: id })
 
-    const result = await this.findProductUseCase.execute({ Id: id })
-
-    return response.status(200).json(result)
+      return response.status(200).json(result)
+    } catch (err) {
+      if (err.name === 'NotFoundError') {
+        return response.status(404).json({
+          title: 'One or more validation errors occurred.',
+          status: 404,
+          errors: [
+            {
+              message: `The product with id '${id}' was not found`
+            }
+          ]
+        })
+      } else {
+        return response.status(404).json({
+          title: 'One or more validation errors occurred.',
+          status: 500,
+          errors: [
+            {
+              message: 'Internal Server Error'
+            }
+          ]
+        })
+      }
+    }
   }
 
   async delete (request: Request, response: Response): Promise<Response> {
     const { id } = request.params
+    try {
+      const result = await this.deleteProductUseCase.execute({ Id: id })
 
-    const result = await this.deleteProductUseCase.execute({ Id: id })
+      return response.status(204).json(result)
+    } catch (err) {
+      if (err.name === 'NotFoundError') {
+        return response.status(404).json({
+          title: 'One or more validation errors occurred.',
+          status: 404,
+          errors: [
+            {
+              message: `The product with id '${id}' was not found`
+            }
+          ]
+        })
+      } else {
+        return response.status(404).json({
+          title: 'One or more validation errors occurred.',
+          status: 500,
+          errors: [
+            {
+              message: 'Internal Server Error'
+            }
+          ]
+        })
+      }
+    }
+  }
 
-    return response.status(204).json(result)
+  async list (request: Request, response: Response): Promise<Response> {
+    const result = await this.listProductUseCase.execute()
+
+    return response.status(200).json(result)
   }
 }
